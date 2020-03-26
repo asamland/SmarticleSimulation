c230919d56bb3f7b92abca0d08e99b2869fc52d1
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-03-26 11:50:20 -0700

    turned off interactive plotting; added multiple colors; removed walls

--- src/static_polar_light_experiment2.py
+++ src/static_polar_light_experiment2.py
@@ -18,8 +18,8 @@ from pdb import set_trace as bp
 
 def init_axes():
 
-    plt.axis('off')
-    plt.ion()
+    # plt.axis('off')
+    # plt.ion()
     fig = plt.figure()
     ax = fig.add_subplot(111, aspect='equal')
     ax.axis([-110, 110, -110, 110,])
@@ -30,14 +30,14 @@ def init_axes():
     return ax
 
 
-def draw_rectangle(ax,xy, th):
+def draw_rectangle(ax,xy, th, color):
     width = 54
     height = 30
     rect_c =(0.749,0.624,1)
     ang= 90+180/np.pi*th
     ax.add_patch(Rectangle(xy=xy ,width=width, height=height, angle = ang,\
-                                linewidth=0, color=rect_c, alpha=0.002))
-    plt.pause(0.0001)
+                                linewidth=0, color=color, alpha=0.002))
+    # plt.pause(0.0001)
 
 GUI = False
 
@@ -48,6 +48,9 @@ ring_path = urdf_dir+'ring.urdf'
 path_80_20 = urdf_dir+'80_20.urdf'
 table_path = urdf_dir+'table/table.urdf'
 flashlight_path = urdf_dir+'flashlight.urdf'
+
+key_fun = lambda x: x[0]
+c = ('red','yellow','blue','white')
 
 
 
@@ -62,7 +65,7 @@ n = 5
 R = [-1.7,1.7,1.7,-1.7]
 L = [1.7,1.7,-1.7,-1.7]
 dt = sim.time_to_steps(0.45)
-time_s = 3*60
+time_s = 8*60
 t_steps = sim.time_to_steps(time_s)
 t0 = time.time()
 
@@ -73,16 +76,15 @@ p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 tableId = p.loadURDF(table_path)
 table_constraint_id = p.createConstraint(tableId,-1,0,-1,p.JOINT_FIXED,[0,0,0],[0,0,0],[0,0,0])
-wall1 = p.loadURDF(path_80_20, basePosition=[-0.2,0.25,z])
-p1c_id = p.createConstraint(wall1,-1,0,-1,p.JOINT_FIXED,[0,0,0],[0,0,0],[0,0,0])
-wall2 = p.loadURDF(path_80_20, basePosition=[0.2,0.25,z])
-p2c_id = p.createConstraint(wall2,-1,0,-1,p.JOINT_FIXED,[0,0,0],[0,0,0],[0,0,0])
+# wall1 = p.loadURDF(path_80_20, basePosition=[-0.2,0.25,z])
+# p1c_id = p.createConstraint(wall1,-1,0,-1,p.JOINT_FIXED,[0,0,0],[0,0,0],[0,0,0])
+# wall2 = p.loadURDF(path_80_20, basePosition=[0.2,0.25,z])
+# p2c_id = p.createConstraint(wall2,-1,0,-1,p.JOINT_FIXED,[0,0,0],[0,0,0],[0,0,0])
 r = p.loadURDF(ring_path, basePosition = [0,0,z])
 smarticles = sim.load_smarticles(n,smarticle_path,[L,R],dt,z)
 for iter in range(runs):
     ax = init_axes()
     plt.title("Run {}".format(1+iter))
-    displacement = []
     p.resetBasePositionAndOrientation(r,[0,0,z],p.getQuaternionFromEuler([0,0,0]))
     ring_constraint_id = p.createConstraint(r,-1,-1,-1,\
                                 p.JOINT_FIXED,[0,0,0],[0,0,0],[0,0,z])
@@ -103,15 +105,23 @@ for iter in range(runs):
             pos,_ = p.getBasePositionAndOrientation(r)
             fl.set_polar_position(pos[:2])
             fl.ray_check(smarticles)
-            if plot_ii %4==0: #plot every 3 s
-                displacement.append(pos[:2])
+            if plot_ii %8==0: #plot every 1s
+                states = []
                 for s in smarticles:
                     ring_center = np.array(pos[:2])
                     if s.plank ==1:
-                        xy = 1000*(s.x[:2]-ring_center)
-                        th = s.x[-1]
-                        #bp()
-                        draw_rectangle(ax,xy,th)
+                        state = np.zeros(3)
+                        state[:2]=1000*(s.x[:2]-ring_center)
+                        state[2] = s.x[2]
+                        states.append(state)
+                    if len(states)==1:
+                        state = states[0]
+                        draw_rectangle(ax, state[:2], state[2],c[0])
+                    elif len(states)>1 and len(states)<4:
+                        states.sort(key=key_fun)
+                        for c_ii, state in enumerate(states):
+                            draw_rectangle(ax, state[:2], state[2],c[c_ii+1])
+
             plot_ii+=1
         for s in smarticles:
             if i%dt==s.gait_phase:
@@ -119,13 +129,12 @@ for iter in range(runs):
                 #s.move_random_corners()
     p.removeAllUserDebugItems()
     pos, _ = p.getBasePositionAndOrientation(r)
+    total_displacement[iter]=int(1000*pos[1])
+    plt.text(-110,100, "dX: {}".format(total_displacement[iter]))
     plt.axis('off')
     plt.show(block = False)
-    plt.pause(0.01)
-    displacement.append(pos[:2])
-    displacement = 1000*np.array(displacement)
-    total_displacement[iter]=displacement[-1,1]
-    print('\n\ntime:{}, run:{}, displacement:{}'.format(time.time()-t0,iter,total_displacement[iter]))
+    plt.pause(1)
+    print('\n\ntime:{}, run:{}, displacement:{}'.format(time.time()-t0,iter+1,total_displacement[iter]))
 p.disconnect()
 print('Average Displacement: {}'.format(sum(total_displacement)/runs))
 print("Done!")
